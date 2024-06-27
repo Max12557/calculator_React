e2ff41dffeadae7865b8dbd221d73b158a72ca75
Max12557 <max_webdev@outlook.com>
2024-06-27 14:59:28 +0300

    complete design and place randomized circles in the background. Looking nice

--- calculatorReact/src/CalcBackground.jsx
+++ calculatorReact/src/CalcBackground.jsx
@@ -2,26 +2,37 @@ import React from 'react';
 import './style.css';
 
 const CalcBackground = () => {
-    const dotsCount = 20;
+    const dotsCount = 30;
     const dots = [];
 
     // generate the dots randomly
     for (let i = 0; i < dotsCount; i++) {
+
+        let widthAndHeight = Math.random() * 10;
+
         const style = {
-            top: `${Math.random * 100}vh`,
-            bottom: `${Math.random * 100}vw`,
-            animationDelay: `${Math.random() * 5}s`,
+            // randomized position
+            top: `${Math.random() * 100}vh`,
+            bottom: `${Math.random() * 100}vh`,
+            left: `${Math.random() * 100}vw`,
+            right: `${Math.random() * 100}vw`,
+            // randomized width and height
+            
+            width: `${widthAndHeight}rem`,
+            height: `${widthAndHeight}rem`,
+            // randomized animation delay
+            animationDelay: `${Math.random() * 2}s`,
         };
 
-        dots.push(<div key={i} classname="dot" style={style}></div>);
-        
+        dots.push(<div key={i} className="dot" style={style}></div>);
+        console.log(dots)
     }
 
     return(
         <div className="background">
             {dots}
         </div>
-    )
+    );
 }
 
 export default CalcBackground
